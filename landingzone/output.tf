a38457bce6e76c839ffee256fedbd40a91f190fd
Himanshu8445 <himshar@microsoft.com>
2024-03-28 13:13:19 +0530

    Deployment of landing & management zone

--- landingzone/output.tf
+++ landingzone/output.tf
@@ -3,37 +3,37 @@ output "resourcegroup" {
   sensitive = true
 }
 
-output "storage_acct" {
-  value     = module.storage_acct
-  sensitive = true
-}
+# output "storage_acct" {
+#   value     = module.storage_acct
+#   sensitive = true
+# }
 
-output "vnet" {
-  value     = module.vnet
-  sensitive = true
-}
+# output "vnet" {
+#   value     = module.vnet
+#   sensitive = true
+# }
 
-output "nsg" {
-  value     = module.nsg
-  sensitive = true
-}
+# output "nsg" {
+#   value     = module.nsg
+#   sensitive = true
+# }
 
-output "route_table" {
-  value     = module.route_table
-  sensitive = true
-}
+# output "route_table" {
+#   value     = module.route_table
+#   sensitive = true
+# }
 
-output "datafactory" {
-  value     = module.datafactory
-  sensitive = true
-}
+# output "datafactory" {
+#   value     = module.datafactory
+#   sensitive = true
+# }
 
-output "databricks-workspace" {
-  value     = module.databricks-workspace
-  sensitive = true
-}
+# output "databricks-workspace" {
+#   value     = module.databricks-workspace
+#   sensitive = true
+# }
 
-output "key-vault" {
-  value     = module.key-vault
-  sensitive = true
-}
+# output "key-vault" {
+#   value     = module.key-vault
+#   sensitive = true
+# }
