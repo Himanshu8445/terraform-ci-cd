da498059310098ce58734e640527b8d34515c096
Himanshu8445 <himshar@microsoft.com>
2024-04-02 12:07:56 +0530

    re-run

--- landingzone-dev/authentication.tf
+++ landingzone-dev/authentication.tf
@@ -5,6 +5,9 @@ terraform {
     container_name       = "tfstate-container"
     key                  = "dlz.tfstate"
     use_oidc             = true
+    tenant_id            = "16b3c013-d300-468d-ac64-7eda0820b6d3"
+    client_id            = "128613ee-6ae3-4671-a358-9bd200c804e5"
+
   }
 
   required_providers {
