708192304e0b1f543f261db3dc1e4820da2ff74e
Himanshu8445 <himshar@microsoft.com>
2024-04-02 12:11:19 +0530

    re-run

--- landingzone-dev/authentication.tf
+++ landingzone-dev/authentication.tf
@@ -16,7 +16,5 @@ terraform {
 
 #####Configure the Azure Provider
 provider "azurerm" {
-  tenant_id = "16b3c013-d300-468d-ac64-7eda0820b6d3"
-  client_id = "128613ee-6ae3-4671-a358-9bd200c804e5"
   features {}
 }
